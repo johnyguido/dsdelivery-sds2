6042bdac17e7ccf8eeec39521271cb8f213e5dd4
johnyguido <johnyguido@hotmail.com.br>
2021-01-15 17:51:06 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,31 +1,41 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+  const navigation = useNavigation();
+  
+  const handleOnPress = () => {
+      navigation.navigate('Home');
+  }
+  
   return (
-    <View style={styles.container}>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
         <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}> DS Delivery</Text>
-          </View>
+        <Text style={styles.text}> DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      backgroundColor: '#DA5C5C',
-      height: 90,
-      paddingTop:50,
-      flexDirection: 'row',
-      justifyContent: 'center',
+    backgroundColor: '#DA5C5C',
+    height: 90,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   text: {
-      fontWeight: 'bold',
-      fontSize:18,
-      lineHeight:25,
-      letterSpacing:-0.24,
-      color:'#FFF',
-      marginLeft:15,
-      fontFamily:'OpenSans_700Bold',
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#FFF',
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold',
 
 
   }
